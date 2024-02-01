b78cf387a306c4fee43ee9c02d217e5c7d6428e0
Felippe Regazio <felippe.moraes@zoho.com>
2024-02-01 02:26:10 -0300

    removed useless attribution

--- wokki.js
+++ wokki.js
@@ -1,83 +1,82 @@
-const wcs = document.querySelectorAll('template[wc-name]')
-  .forEach(template => {
-    const __name = template.getAttribute('wc-name');
-    const __attrs = template.getAttributeNames();
-    const __script = template.content.querySelector('script');
+document.querySelectorAll('template[wc-name]').forEach(template => {
+  const __name = template.getAttribute('wc-name');
+  const __attrs = template.getAttributeNames();
+  const __script = template.content.querySelector('script');
+  
+  template.content.querySelectorAll('script').forEach(el => el.remove());
+  const __interpolations = Array.from(new Set(template.innerHTML.match(/[^{{}}]+(?=}})/gm)));
+
+  window.customElements.define(__name, class extends HTMLElement {
+    $initialized = false;
+    props = {};
+
+    constructor() {
+      super();
+      this.props = this._props();
+    }
     
-    template.content.querySelectorAll('script').forEach(el => el.remove());
-    const __interpolations = Array.from(new Set(template.innerHTML.match(/[^{{}}]+(?=}})/gm)));
+    static get observedAttributes() {
+      return __attrs;
+    }
 
-    window.customElements.define(__name, class extends HTMLElement {
-      $initialized = false;
-      props = {};
+    attributeChangedCallback() {
+      this.props = this._props();
+    }
 
-      constructor() {
-        super();
-        this.props = this._props();
-      }
-      
-      static get observedAttributes() {
-        return __attrs;
-      }
+    _props() {
+      const props = __attrs.reduce((attrs, item) => {
+        if (!item.startsWith('wc-')) {
+          attrs[item] = this.getAttribute(item);
+        }
 
-      attributeChangedCallback() {
-        this.props = this._props();
-      }
+        return attrs;
+      }, {});
 
-      _props() {
-        const props = __attrs.reduce((attrs, item) => {
-          if (!item.startsWith('wc-')) {
-            attrs[item] = this.getAttribute(item);
-          }
+      return Object.freeze(props);
+    }
 
-          return attrs;
-        }, {});
+    _content(interpolations = {}) {
+      let innerHTML = template.innerHTML;
 
-        return Object.freeze(props);
-      }
+      Object.entries(interpolations).forEach(item => {
+        innerHTML = innerHTML.replaceAll(`{{${item[0]}}}`, item[1]);
+      });
 
-      _content(interpolations = {}) {
-        let innerHTML = template.innerHTML;
+      return Object.assign(document.createElement('template'), { innerHTML }).content;
+    }
 
-        Object.entries(interpolations).forEach(item => {
-          innerHTML = innerHTML.replaceAll(`{{${item[0]}}}`, item[1]);
-        });
+    _render() {
+      return new Function(`
+          ${__script.textContent}
+          var $i = {};
 
-        return Object.assign(document.createElement('template'), { innerHTML }).content;
-      }
+          ${__interpolations.map(item => {
+            return `$i["${item}"] = ${item}`;
+          })}
 
-      _render() {
-        return new Function(`
-            ${__script.textContent}
-            var $i = {};
+          return this._content($i);
+      `).bind(this)();
+    }
 
-            ${__interpolations.map(item => {
-              return `$i["${item}"] = ${item}`;
-            })}
+    connectedCallback() {
+      /*
+        * The constructor for a custom element is not supposed to read or write its DOM. 
+        * It shouldn't create child elements or modify attributes. That work needs to be 
+        * done later, usually in a connectedCallback() method (although note that connectedCallback() 
+        * can be called multiple times if the element is removed and re-added to the DOM, 
+        * so you may need to check for this, or undo changes in a disconnectedCallback()).
+        * SPEC: https://html.spec.whatwg.org/multipage/custom-elements.html#custom-element-conformance
+        */
+      !this.$initialized && this.init();
+    }
 
-            return this._content($i);
-        `).bind(this)();
-      }
+    init() {  
+      this.$initialized = true;
+      this.append(this._render());
+    }
 
-      connectedCallback() {
-        /*
-         * The constructor for a custom element is not supposed to read or write its DOM. 
-         * It shouldn't create child elements or modify attributes. That work needs to be 
-         * done later, usually in a connectedCallback() method (although note that connectedCallback() 
-         * can be called multiple times if the element is removed and re-added to the DOM, 
-         * so you may need to check for this, or undo changes in a disconnectedCallback()).
-         * SPEC: https://html.spec.whatwg.org/multipage/custom-elements.html#custom-element-conformance
-         */
-        !this.$initialized && this.init();
-      }
-
-      init() {  
-        this.$initialized = true;
-        this.append(this._render());
-      }
-
-      disconnectedCallback() {
-        // 
-      }      
-    }); 
-  });
+    disconnectedCallback() {
+      // 
+    }      
+  }); 
+});
